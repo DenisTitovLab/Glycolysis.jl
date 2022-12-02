addc9549d39c0625dac07d4a0667a0c0bb0c43e1
Denis-Titov <titov@berkeley.edu>
2022-12-02 00:14:50 -0800

    update glycolysis.jl with stuff

--- src/Glycolysis.jl
+++ src/Glycolysis.jl
@@ -1,5 +1,12 @@
 module Glycolysis
 
 # Write your package code here.
+include("enzyme_rates.jl")
+include("model_parameters.jl")
+include("ODEs.jl")
+
+export glycolysis_ODEs
+export model_params
+export initial_concentrations
 
 end
